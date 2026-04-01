812389321668d960409bb55a97902cb9e16d0ca1
Temesgen Moges <temu1554@gmail.com>
2026-04-01 23:13:00 +0300

    Add Python operators practice exercises

---
+++ operators.py
@@ -0,0 +1,63 @@
+#for qustion 1
+
+a = 10
+b = 5
+sum = a + b
+print(f"the sume of the {a} and {b} is {sum}")
+
+#for qustion 2
+
+a = 15
+b = 4 
+remainder = a % b
+print(f"the remainder of {a} divided by {b} is {remainder}")
+
+#for qustion 3
+
+a = 2
+b = 4
+
+power = a**b
+print(f"{a} raised to the power of {b} is {power}")
+
+#for qustion 4
+a= 10
+b= 7
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater than {a}")
+
+#for qstion 5 
+
+a = 6
+b = 6
+if a == b:
+    print(f"{a} is equal to {b}")
+else:
+    print(f"{a} is not equal to {b}")    
+
+#for qustion 6
+
+x = 8
+x += 4
+print(f"the value of x after adding 4 is {x}")
+
+#for qustion 7
+
+print(5 > 2 and 3 > 7)
+
+#for qustion 8
+print(10 < 5 or 8 > 3)
+
+#for qustion 9
+z = 5
+z *= 3
+print(f"the value of z after multiplying by 3 is {z}")
+
+#for qustion 10
+
+x = 10
+x += 5
+x *= 2
+print(x)
