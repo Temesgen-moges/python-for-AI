4df2a84f9dc2104674f5d0b8a811b9183190a808
Temesgen Moges <temu1554@gmail.com>
2026-04-02 23:02:09 +0300

    Added Python if-statement practice exercises

---
+++ if_Statements.py
@@ -0,0 +1,45 @@
+# #Qstion 1
+# print("please enter your number ")
+# number = int(input())
+
+# if number > 0 and number % 2 == 0:
+#     print("Positive Even")
+# elif number > 0 and number % 2 != 0:
+#     print("Positive Odd")
+# elif number < 0 and number % 2 == 0:
+#     print("Negative Even")
+# elif number < 0 and number % 2 != 0:
+#     print("Negative Odd")
+# elif number == 0:
+#     print("Zero")
+
+# #Qstion 2
+
+# username = "admin"
+# password = "1234"
+# print("please enter your username")
+# input_username = input()
+# print("please enter your password")
+# input_password = input()
+
+# if input_username == username and input_password == password:
+#     print("Login successful")
+# elif input_username != username and input_password == password:
+#     print("Invalid username")
+# elif input_username == username and input_password != password:
+#     print("Invalid password")
+# else:
+#     print("Invalid username and password")
+
+
+#Qstion 3
+
+a = 10
+b = 25
+c = 18
+if a>b and a>c:
+    print (f"{a}")
+elif b>a and b>c:
+    print (f"{b}")
+elif c>b and c>a:
+    print (f"{c}")
