d69ef5d152ad9ad42691c522f608c51018cbae69
Temesgen Moges <temu1554@gmail.com>
2026-04-05 23:56:10 +0300

    Added Python parameters practice exercise

--- parameters.py
+++ parameters.py
@@ -1,7 +1,16 @@
-def calculate_total(price, tax_rate ,descount):
-    tax_amount = price * tax_rate
-    discount_amount = price * descount
-    total_price = price + tax_amount - discount_amount
-    print(f"Total price: {total_price}")
+# def calculate_total(price, tax_rate ,descount):
+#     tax_amount = price * tax_rate
+#     discount_amount = price * descount
+#     total_price = price + tax_amount - discount_amount
+#     print(f"Total price: {total_price}")
 
-calculate_total(100, 0.07, 0.1)    
+# calculate_total(100, 0.07, 0.1)    
+
+
+x = 5
+
+def change():
+    x = 10
+
+change()
+print(x)
